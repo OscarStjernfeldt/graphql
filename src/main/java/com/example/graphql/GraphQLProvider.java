94bee072567c767cef147dc3ccacda6a183edf39
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-05-13 13:53:20 +0200

    Update GraphQLProvider

--- src/main/java/com/example/graphql/GraphQLProvider.java
+++ src/main/java/com/example/graphql/GraphQLProvider.java
@@ -45,7 +45,10 @@ public class GraphQLProvider {
     private RuntimeWiring buildWiring() {
         return RuntimeWiring.newRuntimeWiring()
                 .type(newTypeWiring("Query")
-                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher())
+                        .dataFetcher("shortUrlById",
+                                graphQLDataFetchers.getShortUrlByIdDataFetcher())
+                        .dataFetcher("getAllShortUrls", graphQLDataFetchers.getAllShortUrlDataFetcher()))
                 .type(newTypeWiring("Book")
                         .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
                 .build();
